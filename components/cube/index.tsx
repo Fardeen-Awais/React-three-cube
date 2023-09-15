2d15504f46e87560c8f178814af154467d0b1cb9
fardeen awais <fardeenjutt99@gmail.com>
2023-09-15 05:05:18 -0700

    Add rotation with useFrame

--- components/cube/index.tsx
+++ components/cube/index.tsx
@@ -1,12 +1,13 @@
 'use client'
-import React from 'react'
-import { Canvas } from '@react-three/fiber'
+import React, { useRef } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
 import styles from './style.module.scss'
 const CubeModel = () => {
   return (
     <div className={styles.main}>
         <Canvas>
             <ambientLight intensity={"2"}/>
+            <directionalLight position={[2,1,1]}/>
             <Cube/>
         </Canvas>
     </div>
@@ -14,8 +15,17 @@ const CubeModel = () => {
 }
 
 function Cube() {
+
+    const mesh = useRef()
+    //* This hook allows you to execute code on every rendered frame, like running effects, updating controls, and so on.
+    useFrame((state:any, delta:any) => { 
+        mesh.current.rotation.x += delta * 0.25
+        mesh.current.rotation.y += delta * 0.25
+        mesh.current.rotation.z += delta * 0.25
+      })
+
   return (
-    <mesh>
+    <mesh ref={mesh}>
         <boxGeometry args={["2.5","2.5","2.5"]}/> {/* This basically scale it up to 2.5x2.5x2.5  */}
         <meshStandardMaterial color={"orange"}/>  {/* This basically add color only visible when light apply on cube */}
     </mesh>
