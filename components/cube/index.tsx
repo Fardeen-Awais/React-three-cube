24002d6ac54f9835f8dec5ef3f3e853f327ff66e
fardeen awais <fardeenjutt99@gmail.com>
2023-09-15 05:34:56 -0700

    Complete Cube work ✅✅by ing different image texture

--- components/cube/index.tsx
+++ components/cube/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useRef } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { TextureLoader } from 'three/src/loaders/TextureLoader'
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
+import { TextureLoader} from 'three/src/loaders/TextureLoader'
 import styles from './style.module.scss'
 const CubeModel = () => {
   return (
@@ -18,20 +18,30 @@ const CubeModel = () => {
 function Cube() {
 
     const mesh = useRef()
-    const texture = new TextureLoader().load('/cube.jpg')
     //* This hook allows you to execute code on every rendered frame, like running effects, updating controls, and so on.
     useFrame((state:any, delta:any) => { 
-        mesh.current.rotation.x += delta * 0.25
-        mesh.current.rotation.y += delta * 0.25
-        mesh.current.rotation.z += delta * 0.25
+        mesh.current.rotation.x += delta * 0.75
+        mesh.current.rotation.y += delta * 0.75
+        mesh.current.rotation.z += delta * 0.75
       })
 
-    const texture_1 = new TextureLoader().load('/assets/color.png') // It is a basic red color image texture 
+    const texture_1 = useLoader(TextureLoader,'/assets/1.jpg')
+    const texture_2 = useLoader(TextureLoader,'/assets/2.jpg')
+    const texture_3 = useLoader(TextureLoader,'/assets/3.jpg')
+    const texture_4 = useLoader(TextureLoader,'/assets/4.jpg')
+    const texture_5 = useLoader(TextureLoader,'/assets/5.jpg')
+    const texture_6 = useLoader(TextureLoader,'/assets/6.jpg')
 
+    
   return (
     <mesh ref={mesh}>
         <boxGeometry args={["2.5","2.5","2.5"]}/> {/* This basically scale it up to 2.5x2.5x2.5  */}
-        <meshStandardMaterial map={texture_1}/>  {/* This basically add color only visible when light apply on cube */}
+        <meshStandardMaterial map={texture_1} attach="material-0"/>  {/* This basically add color only visible when light apply on cube */}
+        <meshStandardMaterial map={texture_2} attach="material-1"/>
+        <meshStandardMaterial map={texture_3} attach="material-2"/>
+        <meshStandardMaterial map={texture_4} attach="material-3"/>
+        <meshStandardMaterial map={texture_5} attach="material-4"/>
+        <meshStandardMaterial map={texture_6} attach="material-5"/>
     </mesh>
   )
 }
